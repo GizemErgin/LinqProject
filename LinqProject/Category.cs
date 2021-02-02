18fe1c0a64406c8a80f03a28f8adf8e6d2cc4a5f
gizemergin93@gmail.com <gizemergin93@gmail.com>
2021-02-03 01:11:38 +0300

    Proje dosyası ekle.

---
+++ LinqProject/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqProject
+{
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ LinqProject/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqProject
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public int UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
